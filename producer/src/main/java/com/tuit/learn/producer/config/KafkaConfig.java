8446d2ca9bf4afca2d40bf81b22b4e7080882522
jack <jahon.080879@gmail.com>
2025-03-07 16:52:30 +0500

    add new KafkaConfig for consumer

---
+++ producer/src/main/java/com/tuit/learn/producer/config/KafkaConfig.java
@@ -0,0 +1,49 @@
+package com.tuit.learn.producer.config;
+
+import com.tuit.learn.producer.config.dto.Person;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${kafka.server}")
+    private String kafka;
+
+    @Bean
+    public Map<String,Object> config(){
+        Map<String,Object> map = new HashMap<>();
+        map.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,kafka);
+        return map;
+    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Person> person(){
+        return buildContainerFactory(Person.class);
+    }
+
+    private <T> ConcurrentKafkaListenerContainerFactory<String, T> buildContainerFactory(Class<T> type){
+        JsonDeserializer<T> jsonDeserializer = new JsonDeserializer<>(type,false);
+
+        DefaultKafkaConsumerFactory<String,T> consumerFactory =
+                new DefaultKafkaConsumerFactory<>(config(),
+                        new StringDeserializer(),
+                        jsonDeserializer);
+
+        ConcurrentKafkaListenerContainerFactory<String, T> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+
+}
---
+++ producer/src/main/java/com/tuit/learn/producer/config/dto/Person.java
@@ -0,0 +1,4 @@
+package com.tuit.learn.producer.config.dto;
+
+public record Person(String fullname, Integer age) {
+}
